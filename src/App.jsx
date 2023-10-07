a085e2ab59655cf5a0e1140c36bc4629d71ba73e
CeleryAdams <sallyceleryadams@gmail.com>
2023-10-07 17:14:05 -0400

    add perf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
 
 export default function App()
 {
     return <>
+        <Perf position="top-left"/>
         <OrbitControls makeDefault/>
 
         <mesh>
