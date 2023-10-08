4bf4864643765fa9f7ba9c24adb3746fa6a431a4
CeleryAdams <sallyceleryadams@gmail.com>
2023-10-08 12:51:37 -0400

    load models and set positions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, useGLTF, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
 export default function App()
 {
+    const { nodes } = useGLTF('./models/pitcher_scene.glb')
+    console.log(nodes)
+
     return <>
         <Perf position="top-left"/>
         <OrbitControls makeDefault/>
 
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Center>
+            <mesh geometry={ nodes.glossy.geometry } position={ nodes.glossy.position }>
+                <meshNormalMaterial />
+            </mesh>
+
+            <mesh geometry={ nodes.matte.geometry } position={ nodes.matte.position }>
+                <meshNormalMaterial />
+            </mesh>
+
+            <mesh geometry={ nodes.mirrorFrame.geometry } position={ nodes.mirrorFrame.position }>
+                <meshNormalMaterial />
+            </mesh>
+
+            <mesh geometry={ nodes.mirrorGlass.geometry } position={ nodes.mirrorGlass.position }>
+                <meshNormalMaterial />
+            </mesh>
+
+            <mesh geometry={ nodes.painting.geometry } position={ nodes.painting.position }>
+                <meshNormalMaterial />
+            </mesh>
+
+        </Center>
 
     
     </>
